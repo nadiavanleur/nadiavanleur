34579f1021b9d18334b388779f731650ac8a5296
Nadia van Leur <nadiavl@hotmail.com>
2019-05-14 17:05:34 +0200

    Added favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Link from "gatsby-link";
 import Layout from "../components/layout";
-import Navigation from "../components/navigation";
 
 import "../styles/style.scss";
 
